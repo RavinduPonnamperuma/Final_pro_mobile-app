93a9e109eb25154b1ae28493f91c0633adb31d63
Ravindu Ponnamperuma <90395607+RavinduPonnamperuma@users.noreply.github.com>
2024-10-07 12:37:40 +0530

    add text feld

--- lib/register_page.dart
+++ lib/register_page.dart
@@ -1,14 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
-
 class RegisterPage extends StatelessWidget {
-
-
-
-  final TextEditingController nameController = TextEditingController();
+  final TextEditingController fnameController = TextEditingController();
+  final TextEditingController lnameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController addressController = TextEditingController();
+  final TextEditingController roleController = TextEditingController();
+
 
   RegisterPage({super.key});
   Future<void> registerUser(Map<String, dynamic> user) async {
@@ -37,14 +37,22 @@ class RegisterPage extends StatelessWidget {
         title: const Text('Register'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
-              controller: nameController,
+              controller: fnameController,
               decoration: const InputDecoration(
-                labelText: 'Name',
+                labelText: 'First Name',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: lnameController,
+              decoration: const InputDecoration(
+                labelText: 'Last Name',
                 border: OutlineInputBorder(),
               ),
             ),
@@ -66,13 +74,32 @@ class RegisterPage extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16.0),
+             TextField(
+              controller: addressController,
+              decoration: const InputDecoration(
+                labelText: 'Address',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
+            TextField(
+              controller: roleController,
+              decoration: const InputDecoration(
+                labelText: 'Role',
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
                 // Add your registration logic here
-                String name = nameController.text;
+                String fname = fnameController.text;
+                String lname = lnameController.text;
                 String email = emailController.text;
                 String password = passwordController.text;
-                print('Name: $name, Email: $email, Password: $password');
+                String address = addressController.text;
+                String role = roleController.text;
+                print('First: $fname,Last: $lname, Email: $email, Password: $password,Adress: $address,Role: $role,');
               },
               child: const Text('Register'),
             ),
