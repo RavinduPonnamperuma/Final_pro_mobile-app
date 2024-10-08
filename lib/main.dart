7264ea6a8461b072e4801d064e864d3ccada9117
thimirasadeesha <thimira@maximimpressions.com>
2024-10-08 20:11:57 +0530

    Connected mqtt with iot device and get data from that

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,149 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'login_page.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'dashbord.dart';
+
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 void main() {
-   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  const AndroidInitializationSettings initializationSettingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+  final InitializationSettings initializationSettings = const InitializationSettings(
+    android: initializationSettingsAndroid,
+  );
+  flutterLocalNotificationsPlugin.initialize(initializationSettings);
+
+  runApp(
+   MyApp(),
+
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+  late MqttService mqttService;
+
+  @override
+  void initState() {
+    super.initState();
+    mqttService = MqttService(navigatorKey);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      mqttService.connect();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       debugShowCheckedModeBanner: false,
-      title: 'Login and Register',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: LoginPage(),
     );
+  }
+}
+
+class MqttService {
+  final String serverUri = '15.235.192.41';
+  final int port = 1883;
+  final String clientId = 'flutter_client';
+  final String username = 'sadee';
+  final String password = 'qwerty';
+  final GlobalKey<NavigatorState> navigatorKey;
+  List<String> notifications = [];
+
+
+  MqttServerClient? client;
+
+  MqttService(this.navigatorKey) {
+    client = MqttServerClient(serverUri, clientId);
+    client?.port = port;
+    client?.keepAlivePeriod = 20;
+    client?.onDisconnected = onDisconnected;
+    client?.onConnected = onConnected;
+    client?.logging(on: true);
+
+    final connMessage = MqttConnectMessage()
+        .withClientIdentifier(clientId)
+        .authenticateAs(username, password)
+        .keepAliveFor(20)
+        .withWillTopic('willtopic')
+        .withWillMessage('My Will message')
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce);
+    client?.connectionMessage = connMessage;
+  }
+
+  Future<void> connect() async {
+    try {
+      await client?.connect(username, password);
+      if (client?.connectionStatus?.state == MqttConnectionState.connected) {
+        print('MQTT client connected');
+        client?.subscribe('esp/output1', MqttQos.atMostOnce);
+        client?.subscribe('esp/1/mpu6050', MqttQos.atMostOnce);
+        client?.subscribe('esp/1/adxl345', MqttQos.atMostOnce);
+        client?.subscribe('esp/1/accident', MqttQos.atMostOnce);
+
+        client?.updates!.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+          final String topic = c[0].topic;
+          if (topic == 'esp/output1') {
+            final MqttPublishMessage recMess = c[0].payload as MqttPublishMessage;
+            final String payload = MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+            try {
+              final Map<String, dynamic> message = jsonDecode(payload);
+
+              final double humidity = message['humidity'] ?? 0.0;
+              final double temperature = message['temperature'] ?? 0.0;
+              final double moisture = message['moisture'] ?? 0.0;
+
+              print('Humidity: $humidity');
+              print('Temperature: $temperature');
+              print('Moisture: $moisture');
+
+              final String messageContent = 'Humidity: $humidity, Temperature: $temperature, Moisture: $moisture';
+              print(messageContent);
+
+
+            } catch (e) {
+              print('Failed to decode message: $e');
+            }
+          }
+        });
+
+
+      } else {
+        print('ERROR: MQTT client connection failed - '
+            'status is ${client?.connectionStatus}');
+        client?.disconnect();
+      }
+    } catch (e) {
+      print('Exception: $e');
+      client?.disconnect();
+    }
+  }
+
+
+  void onConnected() {
+    print('Connected to the broker');
+  }
+
+  void onDisconnected() {
+    print('Disconnected from the broker');
+  }
+
+  void disconnect() {
+    client?.disconnect();
   }
 }
